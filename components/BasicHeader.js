b7b2b27a78526e8fa2ef5e9942dc2c37b3ec490e
Ashish Gupta <guptaashish190@gmail.com>
2019-03-24 02:12:34 +0530

    added proptypes

--- components/BasicHeader.js
+++ components/BasicHeader.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+import PropTypes from 'prop-types';
 import Colors from '../constants/Colors';
 
 class BasicHeader extends Component {
@@ -24,6 +25,12 @@ class BasicHeader extends Component {
             </Header>
         );
     }
+}
+
+// PropTypes
+BasicHeader.propTypes = {
+    title: PropTypes.string.isRequired,
+    navigation: PropTypes.object.isRequired,
 }
 
 const styles = StyleSheet.create({
